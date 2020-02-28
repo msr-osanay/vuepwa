6f62efd43196b14e1449b11ac4fb4a6bcb2a2c80
長内勇智 <osanay@msr.co.jp>
2020-02-28 11:55:02 +0900

    release

---
+++ docs/precache-manifest.439d98c8ce3d4653d2ff1317a7cd3df0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a7fa21d993cc23b1ca84",
+    "url": "js/chunk-vendors.d0b0b5e4.js"
+  },
+  {
+    "revision": "2c331e01175d90c4b983",
+    "url": "js/app.4deac7f5.js"
+  },
+  {
+    "revision": "1b380deba9343d76b320",
+    "url": "js/about.39bb3ae6.js"
+  },
+  {
+    "revision": "fe8ec404060c907eb5154ae270f56012",
+    "url": "index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2c331e01175d90c4b983",
+    "url": "css/app.928b75e0.css"
+  }
+];
